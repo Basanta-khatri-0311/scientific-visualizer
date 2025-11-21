f41f58275b453c68794687133dbae56fc86a20e3
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-11-21 10:52:39 +0545

    initial solarsystem page

---
+++ src/pages/SolarSystem.jsx
@@ -0,0 +1,7 @@
+export default function SolarSystem() {
+  return (
+    <div className="min-h-screen bg-[#041019] text-white flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Solar System Visualizer Coming Soon...</h1>
+    </div>
+  );
+}
